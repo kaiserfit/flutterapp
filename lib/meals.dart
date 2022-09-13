76f03a2c5528b6c4a7d92e5ae54edef5acd4a316
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-09-13 15:47:07 +0800

    q123

--- lib/meals.dart
+++ lib/meals.dart
@@ -12,8 +12,183 @@ class Meals extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Meal Plans'),
-        backgroundColor: Colors.redAccent,
+        title: const Text('My Meal Plans & Recipes'),
+        backgroundColor: const Color.fromARGB(255, 166, 44, 43),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.grey.shade500,
+          padding: const EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              //kaiserfitclub
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/covers/kaiserfit-cookbook.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'KaiserFit Cookbooks',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //meal plans for rapid fatloss
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/meal/meal-plans-for-rapid-fat-loss.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    const Text(
+                      'Meal Plans For Rapid Fat Loss',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //naughty clean eating
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/meal/naughty-clean-eating.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    const Text(
+                      'Naughty Clean Eating',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Divine desserts
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/meal/divine-desserts.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    const Text(
+                      'Divine Desserts',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //superfood smoothies
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/meal/superfood-smoothies.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    const Text(
+                      'SuperFood Smoothies',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/programs.dart
+++ lib/programs.dart
@@ -44,13 +44,323 @@ class Programs extends StatelessWidget {
                         Text(
                           'KaiserFit Club',
                           style: TextStyle(
-                            fontSize: 24,
+                            fontSize: 22,
                             fontWeight: FontWeight.w700,
                           ),
                         ),
                         Icon(
                           Icons.arrow_forward_rounded,
                           color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Dream Body in 90
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/covers/dream-body.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Dream Body in 90',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Beach Body
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/covers/beach-body.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Beach Body Blueprint',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Home Workouts
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/covers/home-workouts.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const <Widget>[
+                        Flexible(
+                          child: Text(
+                            'Home Workouts for Rapid Fat Loss',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Meal Plan Masterclass
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/covers/meal-plan.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const <Widget>[
+                        Flexible(
+                          child: Text(
+                            'Meal Plan Masterclass',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Goddess Blueprint
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/programs/goddess-blueprint.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const <Widget>[
+                        Flexible(
+                          child: Text(
+                            'Goddess Blueprint',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Fat loss food hacks
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/programs/foodhacks.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const <Widget>[
+                        Flexible(
+                          child: Text(
+                            'Fat Loss Food Hacks',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              //Forever Fatloss
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey.shade900,
+                  padding: const EdgeInsets.only(bottom: 20),
+                  elevation: 15.0,
+                ),
+                onPressed: () {},
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'images/programs/foreverFL.webp',
+                      fit: BoxFit.fill,
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: 400,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const <Widget>[
+                        Flexible(
+                          child: Text(
+                            'Forever Fat Loss',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Icon(
+                          Icons.arrow_forward_rounded,
+                          color: Colors.red,
+                          size: 30,
                         ),
                       ],
                     ),
