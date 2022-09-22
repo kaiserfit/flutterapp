da753e996958ce63599cb433edca55545d350477
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-09-22 15:21:34 +0800

    updated kaiserfitclub

--- lib/programs/kaiserfitclub.dart
+++ lib/programs/kaiserfitclub.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const KaiserFitClub());
 }
 
-class KaiserFitClub extends StatelessWidget {
+class KaiserFitClub extends StatefulWidget {
   const KaiserFitClub({super.key});
+
+  @override
+  State<KaiserFitClub> createState() => _KaiserFitClubState();
+}
+
+class _KaiserFitClubState extends State<KaiserFitClub> {
+  String title = 'title';
+  int month = 1;
+  int min = 1;
+  int max = 6;
+  int cookbook = 1;
+  int mealplan = 1;
+  int shoppinglist = 1;
+  bool loading = true;
+  String imgpath = 'images/launcher_icon.png';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(mainAxisAlignment: MainAxisAlignment.start, children: const [
+        title: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
           Text(
-            'KaiserFit Club',
-            style: TextStyle(
+            cookbook.toString() +
+                ', ' +
+                mealplan.toString() +
+                ', ' +
+                shoppinglist.toString(),
+            style: const TextStyle(
               fontFamily: 'OpenSansBold',
               fontSize: 22.0,
             ),
           ),
-          SizedBox(
+          const SizedBox(
             width: 20,
           ),
-          Icon(
+          const Icon(
             Icons.star_border_purple500_outlined,
             color: Colors.red,
             size: 30.0,
@@ -32,6 +52,28 @@ class KaiserFitClub extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(12),
         children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    resetIndexes();
+                  });
+                },
+                backgroundColor: Colors.green,
+                child: const Icon(Icons.navigate_before_rounded),
+              ),
+              Text('Month, ${month.toString()}'),
+              FloatingActionButton(
+                onPressed: () {
+                  // Add your onPressed code here!
+                },
+                backgroundColor: Colors.green,
+                child: const Icon(Icons.navigate_next_rounded),
+              ),
+            ],
+          ),
           const SizedBox(
             height: 20,
           ),
@@ -51,14 +93,23 @@ class KaiserFitClub extends StatelessWidget {
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                InkWell(
-                  onTap: () {},
-                  splashColor: Colors.red,
-                  child: Image.asset(
-                    'images/programs/kaiserfitclub/cookbook/1.webp',
-                    fit: BoxFit.contain,
-                  ),
-                ),
+                (loading == true)
+                    ? InkWell(
+                        onTap: () {},
+                        splashColor: Colors.red,
+                        child: Image.asset(
+                          'images/programs/kaiserfitclub/cookbook/1.webp',
+                          fit: BoxFit.contain,
+                        ),
+                      )
+                    : Hero(
+                        tag: imgpath,
+                        child: Image.asset(
+                          imgpath,
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
                 const SizedBox(
                   width: 30,
                 ),
@@ -106,5 +157,23 @@ class KaiserFitClub extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void resetIndexes() {
+    mealplan = 1;
+
+    while ((cookbook == shoppinglist || cookbook == mealplan) &&
+        mealplan == shoppinglist) {
+      cookbook = getRandomNum();
+      mealplan = getRandomNum();
+      shoppinglist = getRandomNum();
+    }
+  }
+
+  int getRandomNum() {
+    Random rnd;
+    rnd = Random();
+    int r = min + rnd.nextInt(max - min);
+    return r;
   }
 }
