6b7c50f635053b1db42c33f945b3d2803065d038
KaiserFit <108109987+kaiserfit@users.noreply.github.com>
2022-09-23 13:46:13 +0800

    Update kaiserfitclub.dart

--- lib/programs/kaiserfitclub.dart
+++ lib/programs/kaiserfitclub.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 // import 'package:lottie/lottie.dart';
-
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 void main() {
   runApp(const KaiserFitClub());
 }
